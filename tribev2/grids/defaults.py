bc62e03d136901bed0eeda583c46bfcaac073d57
Alchaima Sikebir <130755445+Alaska96@users.noreply.github.com>
2026-07-02 09:35:26 +0200

    Update defaults.py

--- tribev2/grids/defaults.py
+++ tribev2/grids/defaults.py
@@ -50,6 +50,7 @@ video_feature = image_feature | {
     "image": {
         "name": "HuggingFaceImage",
         "model_name": "facebook/vjepa2-vitg-fpc64-256",# # facebook/vjepa2-vitl-fpc64-256
+        "device": "cuda",
         "infra": {"keep_in_ram": False},
         "layers": [0.75, 1.0],
     },
@@ -101,7 +102,7 @@ for extractor in [
         extractor["infra"]["slurm_constraint"] = SLURM_CONSTRAINT
         #extractor["device"] = "cuda" # !!!!!!!!!!!!!!!!!!! solve the issue of using CPU intead of GPU , alternative solution was to launch run from inside the compute node instead of log in node
     if extractor["name"] == "HuggingFaceVideo":
-        extractor["device"] = "cuda"
+       # extractor["device"] = "cuda"
         extractor["infra"]["min_samples_per_job"] = 100 ## was 1
         extractor["infra"]["max_jobs"] = 8 #  was 1024 --> QOS limit
         extractor["infra"]["timeout_min"] = 60 * 24*2
