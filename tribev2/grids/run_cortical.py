67f997e331c10c72ab9339e3fd8e446e3bf85d90
Alchaima Sikebir <130755445+Alaska96@users.noreply.github.com>
2026-07-12 07:52:45 +0200

    Update run_cortical.py

--- tribev2/grids/run_cortical.py
+++ tribev2/grids/run_cortical.py
@@ -27,7 +27,7 @@ grid = {# contains grid of the tribev1
     "data.layer_aggregation": [None, "group_mean"],
     "brain_model_config.subject_embedding": [True, False],
     "brain_model_config.extractor_aggregation": ["cat", "mean"],
-    "brain_model_config.feature_aggregation": ["cat", "sum"],
+    #"brain_model_config.feature_aggregation": ["cat", "sum"],
     "brain_model_config.modality_dropout": [0.0, 0.2, 0.4],
 }
 
