0f5536dbe23f60109d6b7ea6f091d5b9c2092519
Alchaima Sikebir <130755445+Alaska96@users.noreply.github.com>
2026-08-16 20:17:08 +0200

    Update defaults.py

--- tribev2/grids/defaults.py
+++ tribev2/grids/defaults.py
@@ -260,7 +260,7 @@ default_config = {
     "patience": None,
     "enable_progress_bar": True,
     "log_every_n_steps": 5,
-    "fast_dev_run": False #True, #False,# PyTorch Lightning flag — if True, runs 1 batch of train+val then stops. For quick sanity checks
+    "fast_dev_run": False,#True, #False,# PyTorch Lightning flag — if True, runs 1 batch of train+val then stops. For quick sanity checks
     "seed": 33,
 }
 
