1cfa6a364b9eadf0eb2128be9d08c569a451c967
Alchaima Sikebir <130755445+Alaska96@users.noreply.github.com>
2026-08-29 23:08:10 +0200

    Update defaults.py

--- tribev2/grids/defaults.py
+++ tribev2/grids/defaults.py
@@ -16,7 +16,7 @@ WANDB_ENTITY = os.getenv("WANDB_ENTITY", "alaska0-university-of-milano-bicocca")
 DATADIR = "/scratch_share/islab/Chaima/tribe_v1_work_space/Data/CMD_Data"
 BASEDIR = "/scratch_share/islab/Chaima/tribe_v2_work_space"
 CACHEDIR = os.path.join(BASEDIR, "cache", PROJECT_NAME)
-SAVEDIR = os.path.join(BASEDIR, "results", PROJECT_NAME)
+SAVEDIR = os.path.join(BASEDIR, "results_partial_cache", PROJECT_NAME)
 
 N_CPUS = 8  #20 # may need to be changed if it violate QOS policy
 
