f53f6d212d2c2e34f306d4c0785149806d906050
Alchaima Sikebir <130755445+Alaska96@users.noreply.github.com>
2026-09-17 22:11:42 +0200

    Update defaults.py

--- tribev2/grids/defaults.py
+++ tribev2/grids/defaults.py
@@ -255,7 +255,7 @@ default_config = {
             },
         },
     },
-    "n_epochs": 15,
+    "n_epochs": 20,
     "limit_train_batches": None, # ,# if set to e.g. 10, only runs 10 batches per epoch — useful for debugging. None = full dataset
     "patience": None, #was none in default sett, 3 matches paper description
     "enable_progress_bar": True,
